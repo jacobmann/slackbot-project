619d9cc14c11a0457723c63a4ed0711b1216a80f
Muzamil Satar <muzamil@mzml.io>
2018-03-10 14:26:10 -0700

    add key and gitignore

--- charlieBot/scripts/list-slack-channels.js
+++ charlieBot/scripts/list-slack-channels.js
@@ -1,23 +1,26 @@
 // Description:
 //    Example scripts for you to examine and try out.
 // commands:
-
-module.exports = function(robot) {
-    robot.hear(/bootcampers/i, function(res) {
-        res.send("It's going");
-    });
-    robot.respond(/list channels/i, function(res) {
-        robot.http("https://slack.com/api/channels.list")
-        .get()(function(err, response, body) {
-          if (err){
-            robot.logger.info(err)
-            res.send("Something went wrong")
-          }else if (response.statusCode===200){
-            res.send(JSON.stringify(body))
-          }else{
-            robot.logger.info(body+response.statusCode)
-            res.send("Something went wrong")
-          }
-        });
+var authKey = require('../../authKey.js')
+module.exports = function (robot) {
+  robot.hear(/bootcampers/i, function (res) {
+    res.send("It's going");
+  });
+  robot.respond(/list channels/i, function (res) {
+    var slackURL = "https://slack.com/api/channels.list?token=" + authKey.authKey
+    robot.http(slackURL)
+      .header('Content-Type', 'application/x-www-form-urlencoded')
+      .get()(function (err, response, body) {
+        if (err) {
+          robot.logger.info(err)
+          res.send("Something went wrong")
+        } else if (response.statusCode === 200) {
+          robot.logger.info(response)
+          res.send(JSON.stringify(body))
+        } else {
+          robot.logger.info(body + response.statusCode)
+          res.send("Something went wrong")
+        }
       });
+  });
 };
